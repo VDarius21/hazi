641b81d55181844aa01b015c958dad306fb452ad
Varga Darius <darovarga@gmail.com>
2025-09-10 13:25:58 +0200

    Atirva magyarra uj branchbe

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,3 @@
 public class Main {
-    public static void main(String[] args) {System.out.println("Szia, szep uj vilag!");}
+    public static void main(String[] args) {System.out.println("Szia, szep uj vilag, uj branch!");}
 }
