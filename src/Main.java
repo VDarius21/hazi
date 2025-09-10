8a8179c5bd2256c1922fb40d9e7bf47835b8bf49
Varga Darius <darovarga@gmail.com>
2025-09-10 13:37:39 +0200

    a+b variable added

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {System.out.println(a()+" "+b());}
 
-    private static String b() {
-        return "world!";
-    }
-
     private static String a() {
         return "Hello,";
+    }
+
+    private static String b() {
+        return "world!";
     }
 }
