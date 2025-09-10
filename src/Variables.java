270529d7defa1909d5557f5472065a3eb0a315fe
Varga Darius <darovarga@gmail.com>
2025-09-10 13:58:37 +0200

    Variables.java have been added with 2 types of usable variables.

---
+++ src/Variables.java
@@ -0,0 +1,12 @@
+public class Variables {
+    public static String greeting = "Hello,";
+    public static String target = "new world!";
+
+    /*public static String a() {
+        return greeting;
+    }
+
+    public static String b() {
+        return target;
+    }*/
+}
